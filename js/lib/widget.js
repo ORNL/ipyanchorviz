99d046d3ced38f13a6bae98e900ffea1efe057c9
Martindale, Nathan <martindalena@ornl.gov>
2023-06-26 14:33:35 -0400

    Update changelog and version

--- js/lib/widget.js
+++ js/lib/widget.js
@@ -10,8 +10,8 @@ export class AnchorVizModel extends DOMWidgetModel {
         _view_name : 'AnchorVizView',
         _model_module : 'ipyanchorviz',
         _view_module : 'ipyanchorviz',
-        _model_module_version : '0.1.0',
-        _view_module_version : '0.1.0',
+        _model_module_version : '0.2.0',
+        _view_module_version : '0.2.0',
 
         /* bi-directional things */
         anchors: [],
